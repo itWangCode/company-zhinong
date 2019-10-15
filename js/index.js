74860b5ee0b531bc925b8917f2b1086f8e36f289
itwangyang@gmail.com <ahao@Macintosh.local>
2019-10-15 16:00:22 +0800

    左边

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+
+  $('#pills-tab .nav-item').click(function(){
+    if($('#pills-tab .nav-item a').is('.active')){
+      $('#pills-tab').removeClass("my-image");
+      $('#pills-tab').addClass("changeImage");
+    }else if($('#pills-tab .nav-item a').is('.active')){
+      $('#pills-tab').removeClass("changeImage");
+      $('#pills-tab').addClass("my-image");
+    }
+  })
+});
