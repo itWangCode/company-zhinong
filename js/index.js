f04e8d4698e5d0a0016a7626149c084ce1310923
itwangyang@gmail.com <ahao@Macintosh.local>
2019-10-17 11:16:26 +0800

    第一个版本

--- js/index.js
+++ js/index.js
@@ -23,4 +23,25 @@ $(document).ready(function () {
     $("#content-right-2").hide();
     $("#content-right-1").show();
   })
+
+
+  // 按钮
+
+  $('input').lc_switch();
+  // triggered each time a field changes status
+  $('body').delegate('.lcs_check', 'lcs-statuschange', function () {
+    var status = ($(this).is(':checked')) ? 'checked' : 'unchecked';
+    console.log('field changed status: ' + status);
+  });
+
+  // triggered each time a field is checked
+  $('body').delegate('.lcs_check', 'lcs-on', function () {
+    console.log('field is checked');
+  });
+
+
+  // triggered each time a is unchecked
+  $('body').delegate('.lcs_check', 'lcs-off', function () {
+    console.log('field is unchecked');
+  });
 });
