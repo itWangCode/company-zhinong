d51e5944b8852023ce3475018a89514c6d48a4aa
itwangyang@gmail.com <ahao@Macintosh.local>
2019-10-17 20:42:58 +0800

    wang

--- js/index.js
+++ js/index.js
@@ -1,27 +1,61 @@
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('#pills-tab .nav-item').click(function(){
-    if($('#pills-tab .nav-item a').is('.active')){
-      $('#pills-tab').removeClass("my-image");
-      // $('#pills-tab').addClass("changeImage");
-    }else if($('#pills-tab .nav-item a').is('.active')){
-      $('#pills-tab').removeClass("changeImage");
-      // $('#pills-tab').addClass("my-image");
+  // $('#pills-tab01 .nav-item').click(function(){
+  //   if($('#pills-tab01 .nav-item a').is('.active')){
+  //     $('#pills-tab01').removeClass("my-image");
+  //     // $('#pills-tab').addClass("changeImage");
+  //   }else if($('#pills-tab01 .nav-item a').is('.active')){
+  //     $('#pills-tab01').removeClass("changeImage");
+  //     // $('#pills-tab').addClass("my-image");
+  //   }
+  // })
+
+  // $('#pills-tab01 .nav-item a').click(function(){
+  //   console.log($('#pills-tab01 .nav-item a').is('.active'));
+  //   if($('#pills-tab01 .nav-item a').is('.active')==true){
+  //     $('#pills-tab01').css('background','url(../img/list/life.png) no-repeat center');
+  //   }else{
+  //     $('#pills-tab01').css('background','url(../img/list/right.png) no-repeat center');
+  //   }
+  // })
+
+  $('#pills-tab01 .nav-item  a').on('click', function (e) {
+    e.preventDefault();
+    console.log($(this).tab('show')[0].id);
+  
+    let isChange = $(this).tab('show')[0].id === 'pills-home-tab0';
+    console.log(isChange);
+    if (isChange===true) {
+      $('#pills-tab01').removeClass("changeImage");
+      $('#pills-tab01').addClass("my-image");
+    } else if (isChange===false) {
+      $('#pills-tab01').removeClass("my-image");
+      $('#pills-tab01').addClass("changeImage");
     }
+
+    $(this).tab('show')
   })
+
+  $(".imgclick").toggle(function () {
+    $(this).attr("src", "img/close.png");
+  }, function () {
+    $(this).attr("src", "img/open.png");
+  }).attr("src", "img/open.png");
 
 
 
   //  id="model-1"
-  $("#model-1").click(function(e){
+  $("#model-1").click(function (e) {
     $("#content-right-2").show();
     $("#content-right-1").hide();
-    e?e.stopPropagation():event.cancelBubble = true; 
+    $(this).attr("src", "../img/click.gif");
+    e ? e.stopPropagation() : event.cancelBubble = true;
   })
 
-  $(".main-bottom-center").click(function() { 
+  $(".main-bottom-center").click(function () {
     $("#content-right-2").hide();
     $("#content-right-1").show();
+    $("#model-1").attr("src", "../img/main/model-pool.png");
   })
 
 
